2746d8d7f227c2dd4d8b5909b5e520f620dbf718
Maciej Janowski <maciej.janowski1@icloud.com>
2024-05-20 15:17:12 -0300

    add read time

--- src/utils/getPostsByTag.ts
+++ src/utils/getPostsByTag.ts
@@ -3,7 +3,7 @@ import getSortedPosts from "./getSortedPosts";
 import { slugifyAll } from "./slugify";
 
 const getPostsByTag = (posts: CollectionEntry<"blog">[], tag: string) =>
-  getSortedPosts(
+  await getSortedPosts(
     posts.filter(post => slugifyAll(post.data.tags).includes(tag))
   );
 
